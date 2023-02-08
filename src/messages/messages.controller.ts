e6c5e1aaa8aef03423b34593387c196ae917dff2
Fischer <fischerrobson@gmail.com>
2023-02-07 23:52:03 -0300

    post method validation with pipe

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDTO } from './DTOs/create-message-dto';
 
 @Controller('/messages')
 export class MessagesController {
@@ -6,7 +7,7 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessage(@Body() body: any) {}
+  createMessage(@Body() body: CreateMessageDTO) {}
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {}
